00b49cca59c1635301abfc4fc699169a767117de
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-08-12 19:18:29 -0700

    parse resume for autofill

---
+++ callAffindaAPI.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from pprint import pprint
+
+from affinda import AffindaAPI, TokenCredential
+from affinda.models import WorkspaceCreate, CollectionCreate
+
+token = os.environ.get("TokenAffindaAPI",None)
+
+file_pth = Path("data/sample.pdf")
+
+credential = TokenCredential(token=token)
+client = AffindaAPI(credential=credential)
+
+my_organisation = client.get_all_organizations()[0]
+
+workspace_body = WorkspaceCreate(
+    organization=my_organisation.identifier,
+    name="My Workspace",
+)
+recruitment_workspace = client.create_workspace(body=workspace_body)
+
+collection_body = CollectionCreate(
+    name="Resumes", workspace=recruitment_workspace.identifier, extractor="resume"
+)
+resume_collection = client.create_collection(collection_body)
+
+with open(file_pth, "rb") as f:
+    resume = client.create_document(file=f, file_name=file_pth.name, collection=resume_collection.identifier)
+
+pprint(resume.as_dict())
