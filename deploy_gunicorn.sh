7d66c32cb81da744d04d680afd99f31ac7b3f209
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-08-15 15:00:21 -0700

    Update deploy_gunicorn.sh
    add ssl

--- deploy_gunicorn.sh
+++ deploy_gunicorn.sh
@@ -1,1 +1,1 @@
-nohup gunicorn -c gunicorn_config.py app:app 2> nohub.out &
+nohup gunicorn -c gunicorn_config.py app:app --certfile=/etc/pki/tls/certs/nftprofilebuilder_com.crt --keyfile=/etc/pki/tls/private/nftprofilebuilder_tld.key 2> nohub.out &
